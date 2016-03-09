f4a02e0b863ee826a1b0cdd8e98b2efb0f55444c
Krypticdator <toni.nurmi@hotmail.com>
2016-03-09 12:46:53 +0200

    Added Setup.py

---
+++ Setup.py
@@ -0,0 +1,12 @@
+__author__ = 'Toni'
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SQLAlchemyBaseClass',
+      version='1.0',
+      description='SQLAlchemy basic operations',
+      author='Toni Nurmi',
+      author_email='toni.nurmi@hotmail.com',
+     )
