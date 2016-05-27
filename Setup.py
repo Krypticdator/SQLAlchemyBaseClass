4f07bb86a7476d5130312bca16cbc453b67822d4
Krypticdator <toni.nurmi@hotmail.com>
2016-05-27 11:37:15 +0300

    updated version number

--- Setup.py
+++ Setup.py
@@ -5,7 +5,7 @@ __author__ = 'Toni'
 from distutils.core import setup
 
 setup(name='SQLAlchemyBaseClass',
-      version='1.2',
+      version='1.3.0',
       description='SQLAlchemy basic operations',
       author='Toni Nurmi',
       author_email='toni.nurmi@hotmail.com',
